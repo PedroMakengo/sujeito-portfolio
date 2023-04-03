b50d2bf7c325a155acbef3c993dce3bd591b694d
PedroMakengo <pedromakengomiguel@gmail.com>
2023-04-03 22:09:49 +0100

    Trabalhando na parte da estrutura do projeto

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from 'next/app'
+
+import { Poppins } from 'next/font/google'
+const poppins = Poppins({ weight: '400', subsets: ['latin'] })
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />
+    </main>
+  )
+}
